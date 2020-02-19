a6574be86421e9a1f9f7bdb74b891face37cfe78
Chainarong Kiddee <chainarong_k@saleecolour.com>
2020-02-19 09:42:51 +0700

    Update Font to Project

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -11,7 +11,7 @@
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
 
-    <link href="https://fonts.googleapis.com/css?family=Sarabun&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Sarabun&display=swap" rel="stylesheet"> -->
     <script src="<?= base_url("js/jquery.min.js") ?>"></script>
     <script src="<?=base_url('js/fontawsome.js')?>"></script>
     <!--
@@ -53,6 +53,43 @@
 
 
     <style>
+    /* thai */
+@font-face {
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Sarabun Regular'), local('Sarabun-Regular'), url(<?=base_url('assets/fonts/DtVjJx26TKEr37c9aAFJn2QN.woff2')?>) format('woff2');
+  unicode-range: U+0E01-0E5B, U+200C-200D, U+25CC;
+}
+/* vietnamese */
+@font-face {
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Sarabun Regular'), local('Sarabun-Regular'), url(<?=base_url('assets/fonts/DtVjJx26TKEr37c9aBpJn2QN.woff2')?>) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+}
+/* latin-ext */
+@font-face {
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Sarabun Regular'), local('Sarabun-Regular'), url(<?=base_url('assets/fonts/DtVjJx26TKEr37c9aBtJn2QN.woff2')?>) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local('Sarabun Regular'), local('Sarabun-Regular'), url(<?=base_url('assets/fonts/DtVjJx26TKEr37c9aBVJnw.woff2')?>) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
         * {
             font-family: 'Sarabun', sans-serif;
         }
@@ -125,7 +162,7 @@ getModal();
                                 <input hidden type="text" name="checkDept" id="checkDept" value="<?= getUser()->DeptCode ?>">
 
                                 <!-- Check Position For use in jQuery ################################################-->
-                                <input type="text" name="checkPosi" id="checkPosi" value="<?= getUser()->posi ?>">
+                                <input hidden type="text" name="checkPosi" id="checkPosi" value="<?= getUser()->posi ?>">
 
 
                                 <div class="widget-content-left  ml-3 header-user-info">
